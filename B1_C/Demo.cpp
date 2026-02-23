27deb9f29105838444c86b03ef44d20f6b7211ef
SauravSarkar-CodersArcade <ronniesaurav17@gmail.com>
2026-02-23 16:55:39 +0530

    Added all latest codes till D3-S3

--- B1_C/Demo.cpp
+++ B1_C/Demo.cpp
@@ -4,13 +4,23 @@ class Sapthagiri{
 public:    
     int x;
     string y;
+    Sapthagiri(int x, string y){
+        cout << "Object has been created!" << endl;
+        this->x = x;
+        this->y = y;
+    }
+    // Destructor
+    ~Sapthagiri() {
+        cout << "Object has been destroyed!" << endl;
+    }
 };
 int main(){
+    
     // new keyword is reserved for DMA in C++
     // it is not used for object creation
-    Sapthagiri obj;
+    Sapthagiri obj(12, "CSE");
     obj.x ;
-    Sapthagiri o = Sapthagiri();
+    Sapthagiri o = Sapthagiri(12, "ISE");
     o.y;
     return 0;
 }
