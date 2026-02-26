c26d601b37e8ccea23f7d6058af935b7e9bde678
SauravSarkar-CodersArcade <ronniesaurav17@gmail.com>
2026-02-26 16:40:59 +0530

    All latest codes till D4-S3

---
+++ SNPSU-B1/src/com/cs_is/garbageCollection/GCDemo.java
@@ -0,0 +1,13 @@
+package com.cs_is.garbageCollection;
+public class GCDemo {
+    public static void main(String[] args) {
+        GCDemo obj = new GCDemo();
+        obj = null;
+        System.gc();
+        // Now-a-days the gc() method is automatically invoked
+        // after program execution by the Object Class
+        // for clutter, garbage collection & removal by the JVM
+        // 🟢 We do not need to call it explicitly
+        System.out.println("GC Requested!");
+    }
+}
---
+++ SNPSU-B1/src/com/cs_is/strings/StringUseCase.java
@@ -0,0 +1,16 @@
+package com.cs_is.strings;
+public class StringUseCase {
+    public static void main(String[] args) {
+        String message = "Order Confirmed";
+        message = message + " - ID: 101";
+        System.out.println(message);
+//        message = message + " - ID: 102";
+//        System.out.println(message);
+    }
+}
+/*
+✅ String is Immutable
+✅ Stored in SCP (String Constant Pool)
+✅ Every modification creates a new Object -> Heap memory
+✅ Good for read-only data
+ */
