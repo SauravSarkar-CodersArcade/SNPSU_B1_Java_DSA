1cd4ca38c7b23c735735f6a05e0dab058e826c3e
SauravSarkar-CodersArcade <ronniesaurav17@gmail.com>
2026-02-28 16:10:55 +0530

    Added all latest codes & PDF notes till D6-S3

--- SNPSU-B1/src/com/cs_is/threads/SafeBankAccount.java
+++ SNPSU-B1/src/com/cs_is/threads/SafeBankAccount.java
@@ -1,29 +1,31 @@
 package com.cs_is.threads;
-public class BankAccount {
+public class SafeBankAccount {
     int balance = 1000;
-    void withdraw(int amount){
+    synchronized void withdraw(int amount){
         balance -= amount;
     }
     public static void main(String[] args) throws InterruptedException {
-        BankAccount acc = new BankAccount();
+        SafeBankAccount acc = new SafeBankAccount();
         Thread t1 = new Thread(() -> acc.withdraw(500));
         Thread t2 = new Thread(() -> acc.withdraw(500));
         t1.start();
         t2.start();
-        // t1.join();
-        // t2.join();
+        t1.join();
+        t2.join();
         // join() will make sure that the main thread
         // waits for the child threads to complete
         System.out.println("Final Balance: " + acc.balance);
     }
 }
 /*
-Both the threads t1 & t2 access & modify the same balance variable
-simultaneously. Due to lack of synchronisation, the final balance becomes
-inconsistent.
+Code Explanation:
+The synchronized keyword ensures that only one thread can execute
+withdraw() operation at a time, preventing RACE CONDITION and ensuring
+correct balance deduction.
+
 ℹ Points to remember:
-⭐ Shared data causes issues
-⭐ Execution order is unpredictable
-⭐ Leads to data inconsistency
-⭐ Need synchronisation to solve the RACE CONDITION ️
+⭐ Synchronisation avoids RACE CONDITION
+⭐ Locks critical section in an application
+⭐ Slower but safe
+⭐ Essential for shared resources ️
  */
